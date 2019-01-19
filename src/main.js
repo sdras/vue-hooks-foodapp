5dd5a467a8ea7173c8e8043ce97ed9408e36dd9e
sdras <sarah.drasner@gmail.com>
2019-01-19 13:39:37 -0700

    add in base hook

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from 'vue';
+import { hooks } from 'vue-hooks';
+import App from './App.vue';
+import store from './store';
 
 Vue.config.productionTip = false
+
+Vue.use(hooks)
 
 new Vue({
   store,
